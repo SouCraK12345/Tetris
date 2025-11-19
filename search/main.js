0e4c36d7336eaa26d375d010ce8b47b61aee7748
SouCraK12345 <souki110212@gmail.com>
2025-11-19 22:20:49 +0900

    ユーザーデータ表示

--- search/main.js
+++ search/main.js
@@ -40,29 +40,55 @@ const msgRef = ref(db, "rate/");
 onValue(msgRef, (snapshot) => {
     if (first) {
         document.querySelector(".init").classList.add("fadeout");
-        document.querySelector(".init").addEventListener("animationend",function(){
+        document.querySelector(".init").addEventListener("animationend", function () {
             document.querySelector(".init").style.display = "none";
-        },{ once: true});
+        }, { once: true });
         window.player_data = snapshot.val();
         let data = window.player_data;
-        for(var i in data){
-            if(i == "undefined" || i == "null"){
+        // "total-point"でソート
+        const sortedKeys = Object.keys(data).sort((a, b) => data[b]["total-point"] - data[a]["total-point"]);
+        let interval = 0;
+        for (var i of sortedKeys) {
+            interval += 25;
+            if (i == "undefined" || i == "null") {
                 continue;
             }
-            document.querySelector(".user-container").innerHTML += `
-              <div class="user-block">
-                <div class="user-name">${i}</div>
-                <div class="user-point">${Math.round(data[i]["total-point"])}pt</div>
-            <div class="user-rank">${getRank(data[i]["total-point"])}</div>
-              </div>
-            `;
+            let i_copy = i;
+            setTimeout(function () {
+                let child = document.createElement("div");
+                child.innerHTML = `
+                    <div class="user-block">
+                        <div class="user-name">${i_copy}</div>
+                        <div class="user-point">${Math.round(data[i_copy]["total-point"])}pt</div>
+                    <div class="user-rank">${getRank(data[i_copy]["total-point"])}</div>
+                </div>
+                `
+                child.addEventListener("click", function () {
+                    document.querySelector(".show-chat").style.display = "block"
+                    document.querySelector("iframe").style.display = "none"
+                    document.querySelector(".user-data-container").classList.add("show");
+                    let name = this.querySelector("div.user-name").innerHTML;
+                    document.querySelector(".image-tile").style.backgroundImage = `url(${data[name]["image"]})`;
+                    document.querySelector(".user-name-label").innerHTML = name;
+                    document.querySelector(".rank").innerHTML = getRank(data[name]["total-point"]);
+                    document.querySelector(".point").innerHTML = `${Math.round(data[name]["total-point"])}pt (${data[name]["point"] >= 0 ? "+" : ""}${Math.round(data[name]["point"])}pt)`;
+                })
+                document.querySelector(".user-container").appendChild(child);
+            }, interval)
         }
     }
     first = false;
 });
+
+function show_chat() {
+    document.querySelector(".show-chat").style.display = "none"
+    document.querySelector("iframe").style.display = "block"
+    document.querySelector("iframe").src = "../chat?to=" + document.querySelector(".user-name-label").innerHTML;
+}
 
 function getRank(e) { if (e < 0) return "範囲外"; if (e <= 299) return "C-"; if (e <= 699) return "C"; if (e <= 1199) return "C+"; if (e <= 1799) return "B-"; if (e <= 2499) return "B"; if (e <= 3499) return "B+"; if (e <= 4499) { if (e >= 3500) return "A"; return "A-" } if (e <= 5499) return "A+"; if (e >= 5500) { const s = Math.floor((e - 5500) / 1e3); if (0 === s) return "S"; if (s >= 1 && s <= 30) return `S+${s}`; if (s > 30) return "S+30" } return "範囲外" }
 
 let first = true;
 
 window.player_data;
+window.show_chat = show_chat;
