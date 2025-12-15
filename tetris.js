57c83fdcde3320f77ea1d9d9e442ba74be9ff853
SouCraK12345 <souki110212@gmail.com>
2025-12-15 05:22:36 +0000

    レートシステム改変を戻す

--- tetris.js
+++ tetris.js
@@ -245,7 +245,7 @@ function game() {
             localStorage.setItem("lose-count", String(Number(RatingSystem.getItem("lose-count")) - 1));
             localStorage.setItem("win-count", String(Number(RatingSystem.getItem("win-count")) + 1));
             RatingSystem.setItem("point", String(Number(RatingSystem.getItem("point")) + win_point));
-            console.log("RatingSystem Updated");
+            // console.log("RatingSystem Updated");
         }
         return false;
     }
@@ -640,7 +640,11 @@ function restart() {
     tet_type = "";
     hold = "";
     able_hold = 1;
-    start_time = new Date();
+    if (((new Date() - start_time) / 1000) < 10 && gearPowers_set[0] == "スタートダッシュ(アタマ)") {
+        console.log("シンプルスタートでのやり直し")
+    } else {
+        start_time = new Date();
+    }
     attack = 0;
     attack2 = 0;
     blocks = 0;
@@ -761,12 +765,14 @@ function Finish(bool = true, clear = true) {
                         document.querySelector(".LINES").innerHTML.replace("Lines: ", ""),
                         document.querySelector(".TIME").innerHTML.replace("Time: ", ""),
                         clear,
-                        virtualbattle_apm,
+                        gamemode == "VirtualBattle" ? virtualbattle_apm : window.enemy_name,
                     ],
                 });
                 xhr.onload = () => {
                     if (xhr.readyState == 4 && xhr.status == 200) {
+                        console.log(xhr);
                         fetch_player_stats();
+                        RatingSystem.update();
                     } else {
                         alert(`Error: ${xhr.status}`);
                     }
@@ -825,7 +831,7 @@ document.addEventListener("keydown", (event) => {
         const banner = document.getElementById("myNotificationBanner");
         banner.click();
     }
-    if (event.key == "f" && document.querySelector("input.retry").checked && gamemode != "Battle") {
+    if (event.key == "f" && document.querySelector("input.retry").checked && gamemode != "Battle" || event.key == "f" && ((new Date() - start_time) / 1000) < 10 && gearPowers_set[0] == "スタートダッシュ(アタマ)") {
         restart();
     }
 });
@@ -960,7 +966,7 @@ document.querySelectorAll(".tile-card").forEach((e) => {
         document.querySelector("#chart-container").style.display = "none";
         document.querySelector("#psb-body").style.display = "none";
         document.querySelector("p.ready").style.display = "block";
-        if (gearPowers_set[0] != "シンプルスタート(アタマ)") document.querySelector("canvas.battle-start").style.display = "block";
+        if (gearPowers_set[0] != "スタートダッシュ(アタマ)") document.querySelector("canvas.battle-start").style.display = "block";
         setTimeout(function () {
             battle_start_update();
             document.querySelector("canvas#tetris").classList.add("show");
@@ -968,7 +974,6 @@ document.querySelectorAll(".tile-card").forEach((e) => {
         setTimeout(function () {
             document.querySelector(".launch").play();
             document.querySelector(".details").style.display = "block";
-            start_time = new Date();
             mainloop();
             if (gamemode != "Watch") {
                 sendData_interval = setInterval(sendData, 750);
@@ -998,7 +1003,7 @@ function back_to_menu() {
             document.querySelector("#psb-matches").scrollTo(0, -1000)
         } catch { }
         document.querySelector(".wipe-in-box").classList.remove("boxWipein");
-        if (user_name) RatingSystem.update();
+        // if (user_name) RatingSystem.update();
     } else {
         document.querySelector(".tile-card-container").style.display = "none";
         document.querySelector("#chart-container").style.display = "none";
